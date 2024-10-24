fef0c7d8360d62dc9b332cd99e6ddab38c0a5e1e
Frez15 <frez1551@gmail.com>
2024-10-24 18:50:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Inicializar Firebase
+// Inicializar Firebase usando variables de entorno
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Importante para formatear el salto de línea
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  })
 });
+
+// Middleware para interpretar JSON en el body de la solicitud
+app.use(express.json());
 
 // Función para enviar notificación push
 function enviarNotificacion(token) {
@@ -16,7 +29,7 @@ function enviarNotificacion(token) {
             title: '¡Timbre tocado!',
             body: 'Alguien ha tocado el timbre.'
         },
-        token: token // El token del dispositivo que recibirá la notificación
+        token: token // Token del dispositivo que recibirá la notificación
     };
 
     admin.messaging().send(message)
