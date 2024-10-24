d00656cd3c10fa95a9e64e68bf647a68e9630acd
Frez15 <frez1551@gmail.com>
2024-10-24 16:10:56 -0300

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -1,11 +1,17 @@
 // api/notify.js
 import admin from 'firebase-admin';
 
-const serviceAccount = require('./serviceAccountKey.json'); //act
-
+// Inicializar Firebase Admin usando variables de entorno
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert({
+            type: process.env.FIREBASE_TYPE,
+            project_id: process.env.FIREBASE_PROJECT_ID,
+            private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+            private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            client_email: process.env.FIREBASE_CLIENT_EMAIL,
+            client_id: process.env.FIREBASE_CLIENT_ID,
+        }),
     });
 }
 
